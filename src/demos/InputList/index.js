a7455e7144c167b4c87af8d6b22302c76202971c
w-t-w <15866369958@163.com>
2024-04-28 10:34:46 +0800

    feature: 受控组件与非受控组件

--- src/demos/InputList/index.js
+++ src/demos/InputList/index.js
@@ -1,10 +1,79 @@
 import {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+
+import './index.css';
+
+class Messages extends PureComponent {
+    static propTypes = {
+        messages: PropTypes.array,
+    };
+
+    static defaultProps = {
+        messages: []
+    }
+
+    render() {
+        const {messages} = this.props;
+        return <ul className="App-demos-input-list-messages">
+            {
+                messages && messages.length ? messages.map((message, index) => <li key={index}>
+                    {message}
+                </li>) : <></>
+            }
+        </ul>
+    }
+}
 
 class InputList extends PureComponent {
+    state = {
+        classmates: '',
+        messages: []
+    };
+
+    /**
+     * 受控组件,完全由外部状态控制,内部不保留 state 以维持状态,输入时,外部控制传入字符,使得输入框内显示输入的字符串
+     * @param event
+     */
+    onInputListChangeHandler = (event) => {
+        const {target: {value}} = event;
+        this.setState({
+            classmates: value
+        });
+        // 取消冒泡事件
+        event.stopPropagation();
+    }
+
+    onInputListSendHandler = (event) => {
+        const {messages, classmates} = this.state;
+        if (classmates) {
+            messages.push(classmates);
+            this.setState({
+                messages: [...messages],
+                classmates: ''
+            });
+        }
+        // 取消冒泡事件
+        event.stopPropagation();
+    };
+
     render() {
-        return <div>
-            我是 InputList
-        </div>
+        const {
+            onInputListChangeHandler,
+            onInputListSendHandler,
+        } = this;
+        const {
+            classmates,
+            messages,
+        } = this.state;
+
+        return <section className="App-demos-input-list">
+            <Messages messages={messages}/>
+            <input className="App-demos-input-list-input" value={classmates}
+                   onChange={(event) => onInputListChangeHandler(event)}/>
+            <button className="App-demos-input-list-button" onClick={(event) => onInputListSendHandler(event)}>
+                Send
+            </button>
+        </section>
     }
 }
 
