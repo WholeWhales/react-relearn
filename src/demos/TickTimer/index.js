0b479dd55aaa994314dbf7b08a5b055e1c9ab16c
w-t-w <15866369958@163.com>
2024-04-29 10:25:31 +0800

    accumulation: think of react

--- src/demos/TickTimer/index.js
+++ src/demos/TickTimer/index.js
@@ -1,10 +1,28 @@
 import {PureComponent} from 'react';
 
 class TickTimer extends PureComponent {
+    state = {
+        timer: new Date()
+    };
+
+    componentDidMount() {
+        console.log('Component did mount!');
+        setInterval(() => this.setState({timer: new Date()}), 1000);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Component did update!');
+    }
+
+    componentWillUnmount() {
+        console.log(`Component will unmount!`);
+    }
+
     render() {
-        return <div>
-            TickTimer
-        </div>
+        const {timer} = this.state;
+        return <section className="App-demos-ticktimer">
+            {timer.toLocaleString()}
+        </section>
     }
 }
 
