b03802d4eb2ca0eac8f479bdfe265ca530951d79
w-t-w <15866369958@163.com>
2024-05-04 16:34:11 +0800

    feature: react demos tabselector

---
+++ src/demos/TabSelector/constants/index.js
@@ -0,0 +1,21 @@
+const options = [{
+    id: 'red',
+    label: 'Red',
+    value: 'red',
+}, {
+    id: 'green',
+    label: 'Green',
+    value: 'green',
+}, {
+    id: 'orange',
+    label: 'Orange',
+    value: 'orange',
+}, {
+    id: 'golden',
+    label: 'Golden',
+    value: 'golden',
+}];
+
+export {
+    options
+};
--- src/demos/TabSelector/index.js
+++ src/demos/TabSelector/index.js
@@ -1,10 +1,69 @@
 import {PureComponent} from 'react';
+import PropTypes from 'prop-types';
+import cns from 'classnames';
+
+import {options} from './constants';
+
+import './index.css';
+
+class TabSelectorOptions extends PureComponent {
+    static propTypes = {
+        options: PropTypes.array,
+        value: PropTypes.string,
+        onChange: PropTypes.func,
+    };
+
+    static defaultProps = {
+        options: [],
+        value: '',
+        onChange: (value, event) => {
+        }
+    };
+
+    render() {
+        const {options, value, onChange} = this.props;
+        return options && options.length ? options.map(option => {
+            const {id, value: optionValue, label} = option;
+            return <button
+                key={id}
+                className={cns(
+                    'App-demos-tabSelector-option',
+                    {
+                        'App-demos-tabSelector-option-selected': value === optionValue,
+                        [`App-demos-tabSelector-option-selected-${value}`]: value === optionValue,
+                    }
+                )}
+                onClick={(event) => onChange(optionValue, event)}
+            >
+                {label}
+            </button>;
+        }) : <></>
+    }
+}
 
 class TabSelector extends PureComponent {
+    state = {
+        color: ''
+    };
+
+    onChangeHandler = (value, event) => {
+        this.setState({
+            color: value
+        });
+        // 取消冒泡事件
+        event.stopPropagation();
+    };
+
     render() {
-        return <>
-            TabSelector Component
-        </>
+        const {color} = this.state;
+        const {onChangeHandler} = this;
+        return <section className='App-demos-tabSelector'>
+            <TabSelectorOptions
+                options={options}
+                value={color}
+                onChange={onChangeHandler}
+            />
+        </section>;
     }
 }
 
